02b75a61168ed988621d58848dfd35755c40093e
Geabons <Geabonzo@gmail.com>
2024-10-25 17:34:28 +0200

    Task A and B done

--- week43.js
+++ week43.js
@@ -2,7 +2,7 @@
 // This is a set of exercises.
 // The idea is to practice a few things at a time.
 // You do this by writing your answer after a task is given (see the example).
-// Use the concepts from our sylabus. 
+// Use the concepts from our syllabus.
 
 // DO NOT change the provided code unless the task specifically says to do so.
 
@@ -11,11 +11,11 @@
     Write code to print all the names in the list, one name per line
 */
 console.log("Task: Example");
-const people = ["Tony", "Christian", "Håkon"]
+const people = ["Tony", "Christian", "Håkon"];
 
 for (let index = 0; index < people.length; index++) {
-    let person = people[index];
-    console.log(person);
+  let person = people[index];
+  console.log(person);
 }
 
 /* -----------------------------------------------------------------------------
@@ -31,6 +31,25 @@ for (let index = 0; index < people.length; index++) {
 */
 console.log("Task: A");
 
+function drawTree(height) {
+  let tree = "";
+  let spaces = "";
+  let stars = "";
+
+  for (let i = 0; i < height - 1; i++) {
+    spaces = " ".repeat(height - i - 1);
+    stars = "*".repeat(2 * i + 1);
+    tree += spaces + stars + "\n";
+  }
+
+  let trunkSpaces = " ".repeat(height - 1);
+  tree += trunkSpaces + "x";
+
+  return tree;
+}
+
+console.log(drawTree(5));
+
 /* -----------------------------------------------------------------------------
     Task: B
 
@@ -45,11 +64,25 @@ console.log("Task: A");
 */
 console.log("Task: B");
 
+function drawArrow(size) {
+  let arrow = "";
+
+  for (let i = 1; i <= size; i++) {
+    arrow += "* ".repeat(i) + "\n";
+  }
+  for (let i = size - 1; i > 0; i--) {
+    arrow += "* ".repeat(i) + "\n";
+  }
+
+  return arrow;
+}
+
+console.log(drawArrow(3));
 /* -----------------------------------------------------------------------------
     Task: C
-    Write a function that draws a box of n by m dimensions. Take into acount the diffrence in aspectratio.
+    Write a function that draws a box of n by m dimensions. Take into account the difference in aspect-ratio.
 
-    Example: This is a aproximatly a 2 by 2 box. 
+    Example: This is a approximately a 2 by 2 box. 
     +--------+
     |        |
     |        |
@@ -57,7 +90,7 @@ console.log("Task: B");
 */
 console.log("Task: C");
 
-
+function drawBox(n, m) {}
 
 /* -----------------------------------------------------------------------------
     Task: D
@@ -65,10 +98,12 @@ console.log("Task: C");
 */
 console.log("Task: D");
 
-
+function heterogramWord(word) {}
 
 /* -----------------------------------------------------------------------------
     Task: E
     Write a function that returns true if two words are Anagrams.
 */
 console.log("Task: E");
+
+function isAnagram(word) {}
